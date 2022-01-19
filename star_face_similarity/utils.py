d95fc733fefb029b8bb99d68b5aac5a4fdcf8a84
Kuroi_Cc <ChenchengMiKuRo@gmail.com>
2022-01-19 19:14:37 +0900

    crawling用ツール半分完成

--- star_face_similarity/utils.py
+++ star_face_similarity/utils.py
@@ -1,7 +1,8 @@
 from typing import List, Tuple
 
-import numpy as np
 import json
+import requests
+import numpy as np
 
 
 def save_image_json(face_names: list, face_encoding: List[np.ndarray], file_path: str):
@@ -24,3 +25,13 @@ def load_image_json(file_path: str) -> Tuple[list, list]:
         face_name_list, face_encoding_list = zip(*json.load(f))
 
     return face_name_list, face_encoding_list
+
+
+# ?是不是应该提供一种从文件夹导入图片的方法
+def crawling_image(name: str, image_url: str, comment: str = '', path: str = 'images'):
+    # TODO: 现在只实现了保存下载图片，还得把数据存到csv里去
+
+    res = requests.get(image_url)
+    extension = image_url.rsplit('.')[-1]
+    with open(f'{path}/{name}.{extension}', 'wb') as f:
+        f.write(res.content)
